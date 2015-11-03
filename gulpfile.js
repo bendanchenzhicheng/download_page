55207135cb9c862ec8fb5d4d91ff573b1364817b
Aaron <315485099@qq.com>
2015-11-03 18:04:07 +0800

    add js html css compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
 	server = require('gulp-server-livereload'),
-	sass = require('gulp-ruby-sass');
+	sass = require('gulp-ruby-sass'),
+  htmlmin = require('gulp-htmlmin'),
+  uglify = require('gulp-uglify'),
+  cssmin = require('gulp-cssmin'),
+  del = require('del');
 
 gulp.task('sass', function () {
   return sass('app/.scss/*.scss')
@@ -22,6 +26,32 @@ gulp.task('server', function() {
 gulp.task('watch:sass', function(){
   gulp.watch('app/.scss/*.scss', ['sass'])
 });
-	
+  
 gulp.task('default', function() {
 });
+
+
+gulp.task('htmlmin', function() {
+  return gulp.src('app/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('cssmin', function () {
+    gulp.src('app/css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('jsmin', function() {
+  return gulp.src('app/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('clean', function() {
+    del(['dist/css', 'dist/js', 'dist/*.html']);
+});
+
+gulp.task('compress', ['htmlmin', 'cssmin', 'jsmin']);
+
